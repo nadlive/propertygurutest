c92e9d57ee941b23ddcb94ac0b5e9d2c23581eac
myview12345 <nadeera.vista@gmail.com>
2013-04-10 12:41:12 +0800

    Algorithem with null logical decorations

---
+++ NumberRangeFilterOne.php
@@ -0,0 +1,18 @@
+<?php
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of NumberRangeFilterOne
+ * @tutorial  this is the class produce blank show for no decoratord Filter
+ * @author nadeera
+ */
+class NumberRangeFilterOne implements InterfaceNumberRangeFilter {
+    
+    public function showResult() {
+        return '';
+    }
+}
