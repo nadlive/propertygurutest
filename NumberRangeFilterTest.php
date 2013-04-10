e0ece7b6a7f8f13cd29d0899756d5ed4dc5d79b8
myview12345 <nadeera.vista@gmail.com>
2013-04-10 12:34:18 +0800

    unit test for the for final output after decorating the initial number range with differnet logic algorithems

--- NumberRangeFilterTest.php
+++ NumberRangeFilterTest.php
@@ -13,22 +13,76 @@
 
 //require_once 'PHPUnit.php';
 require_once 'PHPUnit/Autoload.php';
-
+require_once 'FizzBuzzFilter.php';
+require_once 'FizzBuzzBazzFilter.php';
 
 
 class NumberRangeFilterTest extends PHPUnit_Framework_TestCase {
-
-    function setUp() {
-       
+    
+    public function testBuzzFizzShowResult(){
+       $numberRangeFizBuzz = new FizzBuzzFilter();
+       $expected = " Fizz 13 14 Fizz Buzz 16";
+       $this->assertEquals($expected,$numberRangeFizBuzz->showResult(12,16));
+    }
+    
+    public function testBuzzFizzShowResultWithNegativeInputs(){
+        $numberRangeFizBuzz = new FizzBuzzFilter();
+        $expected = " -1 Fizz Buzz 1 2 Fizz 4 Buzz Fizz 7 8 Fizz Buzz 11 Fizz 13 14 Fizz Buzz 16";
+        $this->assertEquals($expected,$numberRangeFizBuzz->showResult(-1,16));
+    }
+    
+    public function testBuzzFizzShowResultWithOneNullInputs(){
+        $numberRangeFizBuzz = new FizzBuzzFilter();
+        $this->assertFalse($numberRangeFizBuzz->showResult(null,16));
+    }
+    
+    public function testBuzzFizzShowResultWithBothNullInputs(){
+        $numberRangeFizBuzz = new FizzBuzzFilter();
+        $this->assertFalse($numberRangeFizBuzz->showResult(null,null));
+    }
+    
+    public function testBuzzFizzShowBothInputsAreSame(){
+        $numberRangeFizBuzz = new FizzBuzzFilter();
+        $expected = " 1";
+        $this->assertEquals($expected, $numberRangeFizBuzz->showResult(1,1));
+    }
+    
+    public function testBuzzFizzShowResultStartGreaterThanEndValue(){
+        $numberRangeFizBuzz = new FizzBuzzFilter();
+        $this->assertFalse($numberRangeFizBuzz->showResult(100,1));
     }
 
     
-    function tearDown() {
-      
+    public function testBuzzFizzBazzShowResult(){
+       $numberRangeFizBuzz = new FizzBuzzBazzFilter();
+       $expected = " 4 Buzz Fizz Bazz 8 Fizz Buzz Bazz";
+      // $this->assertEquals($expected,$numberRangeFizBuzz->showResult(4,11));
     }
     
-    public function testShowResult(){
-
+    public function testBuzzFizzBazzShowResultWithNegativeInputs(){
+        $numberRangeFizBuzz = new FizzBuzzBazzFilter();
+        $expected = " -1 Fizz Buzz 1 2 Fizz 4 Buzz Fizz Bazz 8 Fizz Buzz Bazz Fizz 13 14 Fizz Buzz 16";
+        $this->assertEquals($expected,$numberRangeFizBuzz->showResult(-1,16));
     }
-
+    
+    public function testBuzzFizzBassShowResultWithOneNullInputs(){
+        $numberRangeFizBuzz = new FizzBuzzBazzFilter();
+        $this->assertFalse($numberRangeFizBuzz->showResult(null,16));
+    }
+    
+    public function testBuzzFizzBazzShowResultWithBothNullInputs(){
+        $numberRangeFizBuzz = new FizzBuzzBazzFilter();
+        $this->assertFalse($numberRangeFizBuzz->showResult(null,null));
+    }
+    
+    public function testBuzzFizzBazzShowBothInputsAreSame(){
+        $numberRangeFizBuzz = new FizzBuzzBazzFilter();
+        $expected = " 1";
+        $this->assertEquals($expected, $numberRangeFizBuzz->showResult(1,1));
+    }
+    
+    public function testBuzzFizzBazzShowResultStartGreaterThanEndValue(){
+        $numberRangeFizBuzz = new FizzBuzzFilter();
+        $this->assertFalse($numberRangeFizBuzz->showResult(100,1));
+    }
 }
