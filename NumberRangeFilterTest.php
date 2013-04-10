515b1f82cc6db571b1e63eae5d0d56a07efede42
myview12345 <nadeera.vista@gmail.com>
2013-04-10 10:34:08 +0800

    adding test class to repository

---
+++ NumberRangeFilterTest.php
@@ -0,0 +1,34 @@
+<?php
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of NumberRangeFilterTest
+ *
+ * @author nadeera
+ */
+
+//require_once 'PHPUnit.php';
+require_once 'PHPUnit/Autoload.php';
+
+
+
+class NumberRangeFilterTest extends PHPUnit_Framework_TestCase {
+
+    function setUp() {
+       
+    }
+
+    
+    function tearDown() {
+      
+    }
+    
+    public function testShowResult(){
+
+    }
+
+}
